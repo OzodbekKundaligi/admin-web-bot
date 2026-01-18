d56310cb8ca76bbbf053db85abfd22a7c8f1dfb3
Ozodbek Mamatov <tradyumuz@gmail.com>
2026-01-18 19:08:22 +0500

    Update db.py

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ from bson import ObjectId
 logging.basicConfig(level=logging.INFO)
 
 # MongoDB ulanish
-MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb+srv://ozod:ozod@garajhub.zqcgpxs.mongodb.net/?appName=GarajHub')
+MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://mongo:uRJdVYymiAPURpSZQKWFUgyTzGzkLHZf@:27017')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'garajhub')
 
 client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=5000)
